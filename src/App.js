d265afc983c283049b79931895fd2fd8367a1a5a
Akash S <108513566+akashnb0@users.noreply.github.com>
2024-08-27 18:13:31 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
      
-      <img src="https://rssmahilacollege.org/assets/img/undr.gif" alt="Loading GIF" />
+      <img src="https://www.techcounsellor.com/wp-content/uploads/2018/11/maintenance-gif-9.gif" alt="Loading GIF" />
     </div>
   );
 }
