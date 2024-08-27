4b8a7d91e93312dfd6da361d23d8536f2c37c866
Akash S <108513566+akashnb0@users.noreply.github.com>
2024-08-27 18:01:42 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,5 @@
 import React from 'react';
-import './App.css';
-import sindhujaImage from './assets/sindhuja.jpg';
-import rakeshImage from './assets/rakeshh.jpg';
-import manishaImage from './assets/manisha.jpg';
 
-function App() {
-  const handleButtonClick = () => {
-    window.location.href = 'https://script.google.com/a/~/macros/s/AKfycbxYrCO_tTOaeYQ_F_c-zQnnow59BbvJieJF-sGkS7w4viu1EphpEeGorPuZSoInzLzU/exec';
-  };
 
   return (
     <div className="App">
