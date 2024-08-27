c99cc22340ec62fcda97ac4e728e80507ef5c1d8
Akash S <108513566+akashnb0@users.noreply.github.com>
2024-08-27 18:08:39 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
      
-      <img src="https://cdn.dribbble.com/users/424937/screenshots/5310986/media/4157627d0ff52468ce017edf62719bec.gif" alt="Loading GIF" />
+      <img src="https://rssmahilacollege.org/assets/img/undr.gif" alt="Loading GIF" />
     </div>
   );
 }
